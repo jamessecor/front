14df4ecb880f63f2c00546a9fe4f14a4febc9456
James Secor <james.secor@gmail.com>
2017-04-17 22:23:17 -0400

    summed sales through db

--- artwork.php
+++ artwork.php
@@ -17,13 +17,14 @@ if(isLoggedIn()) {
 	$person = mysqli_query($db, $query);
 	
 	if($person) {
-		$id = mysqli_fetch_array($person);
+		$id_array = mysqli_fetch_array($person);
+		$id = $id_array[0];
 		
 		// Get member's artwork info from database
-		$query = "SELECT a.title, a.price, CONCAT(p.firstname, ' ', p.lastname) AS 'buyer'
+		$query = "SELECT a.title, a.medium, a.yearMade, a.price, CONCAT(p.firstname, ' ', p.lastname) AS 'buyer'
 				  FROM artwork a 
 				  LEFT OUTER JOIN people p ON a.buyerID = p.personID
-				  WHERE a.artistID = ${id[0]};";
+				  WHERE a.artistID = $id;";
 		$result = mysqli_query($db, $query);
 		
 		if(!$result) {
@@ -33,31 +34,46 @@ if(isLoggedIn()) {
 			
 			print "<table id='memberart'>";
 
-				print "<tr><th>Title</th><th>Price</th><th>Sold To</th></tr>";
+				print "<tr><th>Title</th><th>Medium</th><th>Year</th><th>Price</th><th>Sold To</th></tr>";
 				for($i = 0; $i < $numrows; $i++) {
 					$row = mysqli_fetch_assoc($result);
 					if($row) {
 						$title = $row['title'];
+						$media = $row['medium'];
+						$y     = $row['yearMade'];
 						$price = $row['price'];
 						$buyer = $row['buyer'];
 						if(!$buyer)
 							$buyer = 'n/a';
-						print "<tr><td>$title</td><td>$$price</td><td>$buyer</td></tr>";
+						print "<tr><td>$title</td><td>$media</td><td>$y</td><td>$$price</td><td>$buyer</td></tr>";
 					}
 				}
 			print "</table>";
 		}
+		
+		// Select totals using $id
+		$query = "SELECT sum(price) FROM artwork
+				  WHERE buyerID IS NOT NULL
+				  AND artistID = '$id'";
+		$result = mysqli_query($db, $query);
+		if($result) {
+			$sum_array = mysqli_fetch_array($result);
+			$sum = $sum_array[0];
+			if(!$sum) 
+				$sum = 0;
+			$mcut = $sum * .85;
+			print "<p><strong>Total Sales = \$$sum</strong>";
+			print "<br>Your cut = \$$mcut</p>";
+		}
 	}
-	
-	
-	
-	
+		
 	// TODO: get from db
+	
 	$totalSales = 50;
 	if($totalSales > 0) {
 		$lessCommission = .85 * $totalSales;
-		print "<p><strong>Total Sales = \$$totalSales</strong>";
-		print "<br>Your cut = \$$lessCommission</p>";
+		//print "<p><strong>Total Sales = \$$totalSales</strong>";
+		//print "<br>Your cut = \$$lessCommission</p>";
 	}
 	print "<button id='newart'>Submit New Artwork</button>";
 	print "	<script language='JavaScript'>
