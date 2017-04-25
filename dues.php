de175738288a22e35cf678316e7e6365342de8da
James Secor <james.secor@gmail.com>
2017-04-25 12:17:53 -0400

    Dues Processing: moving forward

--- dues.php
+++ dues.php
@@ -14,47 +14,133 @@ require "../includes/frontConnect.php";
 <?php
 if(adminIsUser()) {
 	$errors = array();
+	$validNewDues = false;
+	$validDuesInput = false;
+	$members = array();
+	
+	// ==========================================
+	// New Dues
+	// ==========================================
+	if(isset($_POST['newdues'])) {
+		// Begin Date
+		if(!empty($_POST['begin'])) {
+			$begin = $_POST['begin'];
+		} else {
+			$errors['begin'] = "Date Not Accepted";
+		}
+		
+		// End Date
+		if(!empty($_POST['end'])) {
+			$end = $_POST['end'];
+		} else {
+			$errors['end'] = "Date Not Accepted";
+		}
+		
+		// Amount
+		if(!empty($_POST['amount'])) {
+			$amount = mysqli_real_escape_string($db, trim($_POST['amount']));
+			if($amount === '') $errors['amount'] = "Amount cannot be empty";
+		} else {
+			$errors['amount'] = "Amount cannot be empty";
+		}
+		
+		// Valid if no errors
+		if(count($errors)===0) {
+			$validNewDues = true;
+		}
+	}
+	
+	// ==========================================
+	// Payments
+	// ==========================================
+	if(isset($_POST['sendpayments'])) {
+		// Members Checkboxes
+		if(!empty($_POST['members'])) {			
+			$members = $_POST['members'];
+			if(!$members) {
+				$errors['members'] = "Select 1 or more members";
+			}
+		} else {
+			$errors['members'] = "Select 1 or more members";
+		}
+		
+		// Payment Amount
+		if(!empty($_POST['payamount'])) {			
+			$payamount = $_POST['payamount'];
+			if(!$payamount) {
+				$errors['payamount'] = "Enter an payamount";
+			}
+		} else {
+			$errors['payamount'] = "Enter an payamount";
+		}
+		
+		// Dues Period
+		if(!empty($_POST['duesperiod'])) {			
+			$duesperiod = $_POST['duesperiod'];
+			if(!$duesperiod) {
+				$errors['duesperiod'] = "Select dues period";
+			}
+		} else {
+			$errors['duesperiod'] = "Select dues period";
+		}
+		
+		// Valid if no errors
+		if(count($errors)===0) {
+			$validDuesInput = true;
+		}
+		
+	}
+	
+	// ==========================================
+	// No Errors: Form 1
+	// ==========================================
+	if($validNewDues) {
+		$v = false;
+		$query = "INSERT INTO dues (periodID, begin, end, amount) VALUES (NULL, '$begin', '$end', $amount);";
+		$result = mysqli_query($db, $query);		
+		
+		// Print error on entry or submission success
+		if(!$result) {
+			$errors['begin'] = "Database Error";
+			die("<table><tr><td>Data Entry Error. <a href=''>Please try again.</a></td></tr>");
+		} else {
+			print "<table><tr><td>The dues period has been submitted.</td></tr>";
+		}
+		print "<tr><td><a href='./dues.php'>Back to Dues</a></td></tr></table>";
+	}
+	// ==========================================
+	// No Errors: Form 2
+	// ==========================================	
+	else if($validDuesInput) {
+	
+		
+	} else {
+		
+	// ==========================================
+	// Forms
+	// ==========================================
 	?>
 <form method='post' action=''>
-	<table>
-		<tr><th colspan=2>Dues Payments</th></tr>
-		<tr>
-			<td>Member:</td>
-			<td>Amount:</td>
-		</tr>
-		<tr>
-			<?php				
-			$query = "SELECT CONCAT(firstname, ' ', lastname) AS 'username' FROM people ORDER BY firstname;";
-			$result = mysqli_query($db, $query);
-			if(!$result) {
-				$errors['username'] = "Error in SQL statement." . mysqli_error($db);
-			} else {
-				$numrows = mysqli_num_rows($result);
-				for($i = 0; $i < $numrows; $i++) {
-					$row = mysqli_fetch_assoc($result);
-					if($row) {
-						$username = $row['username'];
-						echo "<tr><td>$username</td><td><input type='text' name='dues'></td></tr>";
-					}
-				}
-			}
-			?>
-			<td><small class='errorText'><?php echo array_key_exists('username',$errors) ? $errors['username'] : ''; ?></small></td>
-		</tr>
-		<tr>
-			<td></td><td><input type='submit' name='sendpayments' value='Send Payments'></td>
-		</tr>
-	</table>
+	<!-- New Dues Form -->
 	<table>
 		<tr><th colspan=3>New Dues</th></tr>
+		<tr>
+			<td colspan=3><small class='errorText'><?php echo array_key_exists('begin',$errors) ? $errors['begin'] : ''; ?></small></td>
+		</tr>
 		<tr>
 			<td>Begin Date</td>
 			<td><input type='date' name='begin'></td>
 			<td>(ex: 1900-01-31)</td>
 		</tr>
 		<tr>
+			<td colspan=3><small class='errorText'><?php echo array_key_exists('end',$errors) ? $errors['end'] : ''; ?></small></td>
+		</tr>
+		<tr>
 			<td>End Date</td>
 			<td><input type='date' name='end'></td>
+		</tr>
+		<tr>
+			<td colspan=3><small class='errorText'><?php echo array_key_exists('amount',$errors) ? $errors['amount'] : ''; ?></small></td>
 		</tr>
 		<tr>
 			<td>Amount</td>	
@@ -64,9 +150,82 @@ if(adminIsUser()) {
 			<td></td><td><input type='submit' name='newdues' value='New Dues'></td>
 		</tr>
 	<table>
+	<hr>
+	<!-- Dues Payments Form -->
+	<table>
+		<tr><th colspan=4>Dues Payments</th></tr>
+		<tr>
+			<td></td><td></td><td><small class='errorText'><?php echo array_key_exists('duesperiod',$errors) ? $errors['duesperiod'] : ''; ?></small></td>
+		</tr>
+		<tr>
+			<td></td><td>Period Begin Date:</td>
+			<td>
+			<select name='duesperiod' value="<?php echo isset($_POST['duesperiod']) ? $_POST['duesperiod'] : ''; ?>">
+			<option value=''>Choose Period</option>
+			<?php
+			$query = "SELECT begin, end, amount FROM dues ORDER BY end DESC;";
+			$result = mysqli_query($db, $query);
+			if(!$result)
+				$errors['duesperiod'] = "Error in SQL statement." . mysqli_error($db);
+			else {
+				$numrows = mysqli_num_rows($result);
+				for($i = 0; $i < $numrows; $i++) {
+					$dues = mysqli_fetch_array($result);
+					if($dues) {
+						$n = $dues[0];
+						print "<option value='$n'>$n</option>";
+					}
+				}
+			}
+			?>
+			</select>
+			</td>
+		<tr>
+			<td></td><td></td><td><small class='errorText'><?php echo array_key_exists('payamount',$errors) ? $errors['payamount'] : ''; ?></small></td>
+		</tr>
+		<tr>
+			<td></td><td>Payment Amount:</td>
+			<td><input type='text' name='payamount'></td>
+		</tr>
+		<tr>
+			<td></td><th>Members:</th><td><small class='errorText'><?php echo array_key_exists('members',$errors) ? $errors['members'] : ''; ?></small></td>
+		</tr>
+			<?php				
+			// Print Member Checkboxes
+			$query = "SELECT CONCAT(firstname, ' ', lastname) AS 'username' FROM people ORDER BY firstname;";
+			$result = mysqli_query($db, $query);
+			if(!$result) {
+				$errors['username'] = "Error in SQL statement." . mysqli_error($db);
+			} else {
+				$numrows = mysqli_num_rows($result);
+				echo "<tr>";
+				// To create columns
+				$t = 1;
+				for($i = 0; $i < $numrows; $i++) {
+					$row = mysqli_fetch_assoc($result);
+					if($row) {
+						$username = $row['username'];
+						echo "<td><input type='checkbox' name='members[]' value='$username'>$username</td>";
+						// Creates 4 columns
+						if($t % 4 == 0) echo "</tr><tr>";
+						$t++;
+					}
+				}
+				echo "</tr>";
+			}
+			?>
+		<tr>
+			<td><small class='errorText'><?php echo array_key_exists('username',$errors) ? $errors['username'] : ''; ?></small></td>
+		</tr>
+		
+		<tr>
+			<td></td><td></td><td><input type='submit' name='sendpayments' value='Send Payments'></td>
+		</tr>
+	</table>	
 </form>
 
 <?php
+	}
 } else {
 	print "<div class='headings'><a href='login.php'>Log In to Proceed</a></div>";
 }
--- memberimages.php
+++ memberimages.php
@@ -14,20 +14,35 @@ function displayImages($query, $memberArray)
 	
 	$data = mysqli_query($db, $query);
 	if(!$data) {
-		print "Images could not be retrieved.";
+		//print "Images could not be retrieved.";
 	} else {
+		// Number of images
 		$numrows = mysqli_num_rows($data);
+		print "<table>";
 		for($i = 0; $i < $numrows; $i++) {
 			$row = mysqli_fetch_assoc($data);
 			if($row) {
 				$filepath = "./uploads/" . $row['filename'];
-				print "<div id='label'><em>$row[title]</em>, $row[yearMade]. $row[medium]<br>$row[member]</div><br>";
-				print "<img width='60%' src='$filepath' alt='No Image'><br><br>";
+				print "<tr><td><em>$row[title]</em>, $row[yearMade]. $row[medium]<br>$row[member]</td></tr>";
+				print "<tr><td><img width='60%' src='$filepath' alt='No Image'></td></tr>";
+				//print "<div id='label'><em>$row[title]</em>, $row[yearMade]. $row[medium]<br>$row[member]</div><br>";
+				//print "<img width='60%' src='$filepath' alt='No Image'><br><br>";
 			}
 		}
+		print "</table>";
 		?>
 		<div style="text-align:center;">
-			<a href='#' id='toTop'>back to top</a>
+			<button id='toTop'>Back to Top</button>
+			<button id="toImgSelection">Back to Image Selection</button>
+			<script>
+			document.getElementById("toImgSelection").addEventListener("click", function() {
+				window.location.href = 'memberImages.php';
+			});
+			
+			document.getElementById("toTop").addEventListener("click", function() {
+				window.location.href = '#';
+			});
+			</script>
 		</div>
 		<br><br>
 		<?php
@@ -57,20 +72,22 @@ if(isLoggedIn()) {
 		$validation = true;
 	}
 	
-	
+	// Show number has been selected
 	if($validation) {
 		if(isset($_POST['gogogo'])) {
-			$query = "	SELECT CONCAT(m.firstname, ' ', m.lastname) AS 'member', a.title, a.yearMade, a.medium, a.filename 
-				FROM artwork a 
-				JOIN people m ON a.artistID = m.personID WHERE a.filename IS NOT NULL;";
-				//WHERE showNumber = '$showNum';";
-		} else {
-			$query = "	SELECT CONCAT(m.firstname, ' ', m.lastname) AS 'member', a.title, a.yearMade, a.medium, a.filename 
+			$query = "SELECT CONCAT(m.firstname, ' ', m.lastname) AS 'member', a.title, a.yearMade, a.medium, a.filename 
 				FROM artwork a 
 				JOIN people m ON a.artistID = m.personID 
-				WHERE showNumber = '$showNum';";
+				WHERE a.filename IS NOT NULL
+				ORDER BY m.firstname;";
+		} else {
+			$query = "SELECT CONCAT(m.firstname, ' ', m.lastname) AS 'member', a.title, a.yearMade, a.medium, a.filename 
+				FROM artwork a 
+				JOIN people m ON a.artistID = m.personID 
+				WHERE showNumber = '$showNum' AND a.filename IS NOT NULL
+				ORDER BY m.firstname;";
 		}
-		displayImages($query, 'J');
+		displayImages($query, 'xxx');  // 2nd parameter (member list) not used
 	} else {
 ?>
 
@@ -88,7 +105,7 @@ if(isLoggedIn()) {
 			<select name='showNumber' value="<?php echo isset($_POST['showNumber']) ? $_POST['showNumber'] : ''; ?>">
 			<option value=''>Choose Show</option>
 			<?php
-			$query = "SELECT showNumber FROM artwork ORDER BY showNumber DESC;";
+			$query = "SELECT showNumber FROM artwork WHERE filename IS NOT NULL ORDER BY showNumber DESC;";
 			$result = mysqli_query($db, $query);
 			if(!$result)
 				$errors['showNumber'] = "Error in SQL statement." . mysqli_error($db);
@@ -103,7 +120,7 @@ if(isLoggedIn()) {
 				}
 			}
 			?>
-			
+			</select>
 			</td>
 			<td>or</td>
 			<td><input type='submit' name='gogogo' value='Show All Images'></td>
