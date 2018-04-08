f711a0d7d815a81e2e4b28d85f45aff54fbb589a
James Secor <james.secor@gmail.com>
2018-04-08 16:10:32 -0400

    working to get add and update users

--- createnewuser.php
+++ createnewuser.php
@@ -9,7 +9,7 @@ $username = $_SESSION['username'];
 ?>
 
 <div id="right_col">
-<div class='headings'>Create New User</div>
+<div class='headings'><?php echo isset($_GET['userid']) ? "Update User" : "Create New User"; ?></div>
 <div class="center">
 <?php
 if(adminIsUser()) {
@@ -17,10 +17,58 @@ if(adminIsUser()) {
 	$errors[0] = "Missing info: ";
 	$errors[1] = false;
 	$validInputs = false;
-	if(isset($_POST['submitNewUser'])) {
+	$firstname  = "";
+	$lastname   = "";
+	$address    = "";
+	$city       = "";
+	$state      = "";
+	$zip        = "";
+	$phone      = "";
+	$usertype   = "";
+	$website    = "";
+	$email      = "";
+	$buttonName = "submitNewUser";
+	$buttonValue= "Submit New Info";
+	if(isset($_GET['userid'])) {
+		$userId = $_GET['userid'];
+		$buttonName = "submitUserUpdate";
+		$buttonValue= "Update User Info";
+		
+		// Select user's info
+		$userInfoQuery = "SELECT * FROM people WHERE personID = '$userId';";
+		$userInfoResults = mysqli_query($db, $userInfoQuery);
+		if(!$userInfoResults || mysqli_num_rows($userInfoResults) > 1) {
+			die("Unable to find user" . mysqli_error());
+		} else {
+			$userInfo  = mysqli_fetch_assoc($userInfoResults);
+			$firstname = $userInfo['firstname'];
+			$lastname  = $userInfo['lastname'];
+			$address   = $userInfo['address'];
+			$city      = $userInfo['city'];
+			$state     = $userInfo['state'];
+			$zip       = $userInfo['zip'];
+			$phone     = $userInfo['phone'];
+			$email     = $userInfo['email'];
+			$usertype  = $userInfo['member'];
+			$website   = $userInfo['website'];
+		}
+		
+		// Set button value
+		
+		// Set query (INSERT or UPDATE)
+		
+	}
+	
+	if(isset($_POST['submitNewUser']) || isset($_POST['submitUserUpdate'])) {
+		// Set parallel arrays for column names and values
+		$columnName = array();
+		$columnValue = array();
+
 		// firstname
 		if(!empty($_POST['firstname'])) {
 			$firstname = addslashes(trim($_POST['firstname']));
+			$columnName[] = "firstname";
+			$columnValue[] = $firstname;
 		} else {
 			$errors[1] = "first name";
 		}
@@ -28,6 +76,8 @@ if(adminIsUser()) {
 		// lastname
 		if(!empty($_POST['lastname'])) {
 			$lastname = addslashes(trim($_POST['lastname']));
+			$columnName[] = "lastname";
+			$columnValue[] = $lastname;
 		} else {
 			$errors[1] = $errors[1] ? "$errors[1], last name" : "last name";
 		}
@@ -35,50 +85,52 @@ if(adminIsUser()) {
 		// address
 		if(!empty($_POST['address'])) {
 			$address = addslashes(trim($_POST['address']));
-		} else {
-			$address = "NULL";
+			$columnName[] = "address";
+			$columnValue[] = $address;
 		}
 		
 		// city
 		if(!empty($_POST['city'])) {
 			$city = addslashes(trim($_POST['city']));
-		} else {
-			$city = "NULL";
+			$columnName[] = "city";
+			$columnValue[] = $city;
 		}
 		
 		// state
 		if(!empty($_POST['state'])) {
 			$state = addslashes(trim($_POST['state']));
-		} else {
-			$state = "NULL";
+			$columnName[] = "state";
+			$columnValue[] = $state;
 		}
 		
 		// zip
 		if(!empty($_POST['zip'])) {
 			$zip = addslashes(trim($_POST['zip']));
-		} else {
-			$zip = "NULL";
-		}	
+			$columnName[] = "zip";
+			$columnValue[] = $zip;
+		}
 		
 		// email
 		if(!empty($_POST['email'])) {
 			$email = addslashes(trim($_POST['email']));
-		} else {
-			$email = "NULL";
+			$columnName[] = "email";
+			$columnValue[] = $email;
 		}
 
 		// phone
 		if(!empty($_POST['phone'])) {
 			$phone = addslashes(trim($_POST['phone']));
-		} else {
-			$phone = "NULL";
+			$columnName[] = "phone";
+			$columnValue[] = $phone;
 		}
 		
 		// usertype
 		if(!empty($_POST['usertype'])) {
 			$usertype= addslashes(trim($_POST['usertype']));
+			$columnName[] = "member";
+			$columnValue[] = $usertype;
 			if(strlen($usertype) == 0) {
-				$errors[1] = $errors[1] ? "$errors[1], user type" : "user type";
+				$errors[1] = $errors[1] ? "$errors[1], user typex" : "user typex";
 			}
 		} else {
 			$errors[1] = $errors[1] ? "$errors[1], user type" : "user type";
@@ -87,8 +139,8 @@ if(adminIsUser()) {
 		// website
 		if(!empty($_POST['website'])) {
 			$website = addslashes(trim($_POST['website']));
-		} else {
-			$website = "NULL";
+			$columnName[] = "website";
+			$columnValue[] = $website;
 		}
 		
 		// If all good, proceed
@@ -97,8 +149,29 @@ if(adminIsUser()) {
 		}
 	}
 	if($validInputs) {
-		$joinDate = date("Ymd");
-		$addUserQuery = "INSERT INTO people (
+		if(isset($_POST['submitUserUpdate'])) {
+			$userid = $_GET['userid'];
+			$q1 = "UPDATE people SET ";
+			$q2 = "";
+			$q3 = "WHERE personID = $userid;";
+			print_r($columnName);
+			print_r($columnValue);
+			for($i = 0; $i < count($columnName); $i++) {
+				if($q2 != "") {
+					$q2 .= ", ";
+				}
+				// Use quotes or not
+				if(is_numeric($columnValue[$i]))
+					$q2 .= $columnName[$i] . " = ${columnValue[$i]} ";
+				else 
+					$q2 .= $columnName[$i] . " = '${columnValue[$i]}' ";
+								
+			}
+			$userQuery = $q1 . $q2 . $q3;
+			echo $userQuery;
+		} elseif(isset($_POST['submitNewUser'])) {
+			$joinDate = date("Ymd");				
+			$userQuery = "INSERT INTO people (
 						firstname, 
 						lastname, 
 						phone, 
@@ -124,7 +197,8 @@ if(adminIsUser()) {
 						'$website',
 						$usertype
 					);";
-		$queryResponse = mysqli_query($db, $addUserQuery);
+		}		
+		$queryResponse = mysqli_query($db, $userQuery);
 		if($queryResponse) {
 			?>
 			<table>
@@ -158,54 +232,55 @@ if(adminIsUser()) {
 			<td colspan=2><small class="errorText"><?php if($errors[1]) echo "$errors[0]$errors[1]"; ?> </small></td>
 		<tr>
 			<td>First Name</td>
-			<td><input type="text" name="firstname"></td>
+			<td><input type="text" value="<?php echo $firstname ? $firstname : '';?>" name="firstname"></td>
 		</tr>
 		<tr>
 			<td>Last Name</td>
-			<td><input type="text" name="lastname"></td>
+			<td><input type="text" value="<?php echo $lastname ? $lastname : '';?>" name="lastname"></td>
 		</tr>
 		<tr>
 			<td>Address</td>
-			<td><input type="text" name="address"></td>
+			<td><input type="text" value="<?php echo $address ? $address : '';?>" name="address"></td>
 		</tr>		
 		<tr>
 			<td>City</td>
-			<td><input type="text" name="city"></td>
+			<td><input type="text" value="<?php echo $city ? $city : '';?>" name="city"></td>
 		</tr>
 		<tr>
 			<td>State</td>
-			<td><input type="text" name="state"></td>
+			<td><input type="text" value="<?php echo $state ? $state : '';?>" name="state"></td>
 		</tr>
 		<tr>
 			<td>Zip Code</td>
-			<td><input type="text" name="zip"></td>
+			<td><input type="text" value="<?php echo $zip ? $zip : '';?>" name="zip"></td>
 		</tr>						
 		<tr>
 			<td>Phone Number</td>
-			<td><input type="text" name="phone"></td>
+			<td><input type="text" value="<?php echo $phone ? $phone : '';?>" name="phone"></td>
 		</tr>
 		<tr>
 			<td>Email</td>
-			<td><input type="email" name="email"></td>
+			<td><input type="email" value="<?php echo $email ? $email : '';?>" name="email"></td>
 		</tr>
 		<tr>
 			<td>Website</td>
-			<td><input type="text" name="website"></td>
+			<td><input type="text" value="<?php echo $website ? $website : '';?>" name="website"></td>
 		</tr>
 		<tr>
 			<td>User Type</td>
 			<td>
 			<select name="usertype">
 				<option value="">Select...</option>
-				<option value="1">member - 1</option>
-				<option value="<?php echo $currentShow; ?>">Guest Artist - <?php echo $currentShow; ?></option>
-				<option value="0">buyer - 0</option>
+				<option value="1" <?php echo ($usertype == 1) ? "selected" : ""; ?>>member (1)</option>
+				<option value="<?php echo $currentShow; ?>" 
+								<?php echo ($usertype != 1 && $usertype != 0) ? "selected" : ""; ?>>Guest Artist (<?php echo $currentShow; ?>)</option>
+				<option value="0" <?php echo (isset($_GET['userid']) && $usertype == 0) ? "selected" : ""; ?>>non-member (0)</option>
 			</select>
 			</td>
 		</tr>
 		<tr>
 			<td></td>
-			<td><input type="submit" name="submitNewUser" value="Submit New Info"> </td>
+			<td><input type="submit" name="<?php echo $buttonName; ?>" value="<?php echo $buttonValue; ?>"> </td>
 		</tr>
 		<tr><td>&nbsp;</td></tr>
 		<tr>
--- usermanagement.php
+++ usermanagement.php
@@ -11,10 +11,50 @@ include "frontHeader.php";
 <?php
 if(adminIsUser()) {
 ?>
-<table>
+<script>
+$(document).ready(function() {
+	$("#selectEditUser").change(function() {		
+		var user = $("#selectEditUser").val();
+		if(user !== "")
+			$("#editLink").html("<a href=\"./createnewuser.php?userid=" + user + "\" >Edit User</a>");
+		else
+			$("#editLink").html("<a href=\"\" >Edit User</a>");
+	});
+});
+</script>
+<table align="center">
 	<tr>
-		<td><a href="./setmemberpassword.php">Set Member Password</td>
-		<td><a href="./createnewuser.php">Create New User</td>
+		<td><a href="./setmemberpassword.php">Set Member Password</a></td>
+	
+		<td><a href="./createnewuser.php">Create New User</a></td>
+	</tr>
+	<tr><td>&nbsp;</td></tr>
+	<tr>
+		<td><select id="selectEditUser" name="username">
+			<option value=''>Choose Name</option>
+			<?php
+			
+			// Use query to get artists to populate username drop-down		
+			$query = "SELECT personID, CONCAT(firstname, ' ', lastname) AS 'username' FROM people ORDER BY username;";
+			$result = mysqli_query($db, $query);
+			if(!$result) {
+				$errors['username'] = "Error in SQL statement." . mysqli_error($db);
+			} else {
+				$numrows = mysqli_num_rows($result);
+				for($i = 0; $i < $numrows; $i++) {
+					$row = mysqli_fetch_assoc($result);
+					if($row) {
+						$username = $row['username'];
+						$userId = $row['personID'];
+						echo "<option value='$userId'>$username</option>";
+					}
+				}
+			}
+			?>
+		</select></td>
+		<tr>
+			<td id="editLink"><a href="">Edit User</a></td>
+		</tr>
 	</tr>
 </table>
 <?php
